9b94b32f134ff4c58cb312ebde49395638991e4d
mth <mark@hibberd.id.au>
2017-08-04 23:20:26 +1000

    Add skeleton for some serialisation code.

--- test/test.hs
+++ test/test.hs
@@ -1,8 +1,11 @@
 import           Disorder.Core.Main
+
 import qualified Test.Ace.Data
+import qualified Test.Ace.Serial
 
 main :: IO ()
 main =
   disorderMain [
       Test.Ace.Data.tests
+    , Test.Ace.Serial.tests
     ]
