35c7620f9253c98ff1055fa126317d6e1d1f416b
Jacob Stanley <jacob@stanley.io>
2017-08-04 23:15:34 +1000

    Data

--- src/Ace/Data.hs
+++ src/Ace/Data.hs
@@ -1,3 +1,27 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Ace.Data (
+    SiteId(..)
+  , River(..)
+  , World(..)
   ) where
+
+import           P
+
+
+newtype SiteId =
+  SiteId {
+      siteId :: Int
+    } deriving (Eq, Ord, Show)
+
+data River =
+  River {
+      riverSource :: SiteId
+    , riverTarget :: SiteId
+    } deriving (Eq, Ord, Show)
+
+data World =
+  World {
+      worldSites :: [SiteId]
+    , worldMines :: [SiteId]
+    , worldRivers :: [River]
+    } deriving (Eq, Ord, Show)
