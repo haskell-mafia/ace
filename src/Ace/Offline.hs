0d23772be2c3158a43ba7093f3e5a118b0b0e8c9
Nick Hibberd <nhibberd@gmail.com>
2017-08-05 20:58:39 +1000

    Log what bot is running to stderr

--- src/Ace/Offline.hs
+++ src/Ace/Offline.hs
@@ -9,13 +9,16 @@ module Ace.Offline (
 
 import           Ace.Data
 
+import qualified Data.Text as Text
+
 import           P
 
 import           System.IO (IO)
-
+import qualified System.IO as IO
 
 setup :: Robot a -> Setup -> IO (SetupResult (State a))
 setup r s@(Setup p c w settings) = do
+  IO.hPutStrLn IO.stderr . Text.unpack $ "Running robot:" <> robotLabel r
   x <- robotInit r s
   pure $ SetupResult p (initialisationFutures x) (State p c w settings $ initialisationState x)
 
